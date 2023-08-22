2a78902e84c41c3a54b991f599455a2500ff4054
Tanmay Dureja <tanmaydureja@gmail.com>
2023-08-21 20:52:45 -0700

    Redesign Homepage

--- src/components/HomepageAboutMe/index.jsx
+++ src/components/HomepageAboutMe/index.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/no-unescaped-entities */
-import chrisimage1 from "../../assets/home/chris-malone.jpg";
+import chrisimage from "../../assets/home/chris-malone.jpg";
+import chrisimagemobile from "../../assets/home/chris-malone-mobile.jpg";
 
 const HomepageAboutMe = () => {
   return (
@@ -7,13 +8,18 @@ const HomepageAboutMe = () => {
       <div className="flex flex-col md:flex-row justify-center items-center max-w-7xl mx-auto">
         <div className="md:w-1/2">
           <img
-            className="w-[25em] mx-auto max-w-2xl rounded-sm shadow-xl bg-white p-4"
-            src={chrisimage1}
+            className="w-[25em] mx-auto max-w-2xl rounded-sm shadow-xl bg-white p-4 hidden md:block"
+            src={chrisimage}
             alt=""
           />
         </div>
+        <img
+            className="w-[25em] mx-auto rounded-sm shadow-xl bg-white p-4 md:hidden"
+            src={chrisimagemobile}
+            alt=""
+        />
         <div className="md:w-1/2">
-          <div className="max-w-xl font-lora text-2xl lea text-white lg:max-w-xl p-2">
+          <div className="text-white max-w-xl font-lora leading-10 md:leading-10 text-lg md:text-2xl pt-10 md:p-4">
             <p>
               I am Chris Malone, a self-taught artist born and raised in the
               Midwest and currently situated near Washington DC. I started making dolls in 1992 and have
