572bbac991f0830bb48207f5126d21e0ede19f25
Tanmay Dureja <tanmaydureja@gmail.com>
2025-04-28 16:47:11 -0700

    Add Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import Footer from "./components/Footer/index";
 import "./index.css";
+import { Analytics } from '@vercel/analytics/next';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
     <Footer />
+    <Analytics />
   </React.StrictMode>
 );
